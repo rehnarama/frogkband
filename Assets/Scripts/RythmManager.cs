f97d4672673d2fa69a4d90b2ca402e88c450c411
Astrid Rehn <1352555+rehnarama@users.noreply.github.com>
2024-09-07 19:03:14 +0200

    Music!

--- Assets/Scripts/RythmManager.cs
+++ Assets/Scripts/RythmManager.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using FluidMidi;
 using IntervalTree;
+using MidiParser;
 using NUnit.Framework;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -11,6 +13,8 @@ using Random = UnityEngine.Random;
 public class RythmManager : MonoBehaviour
 {
     public Note NotePrefab;
+    public StreamingAsset Song;
+    public float NoteOffset;
 
     public List<Instrument> Instruments;
 
@@ -19,16 +23,26 @@ public class RythmManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for (int i = 0; i < 50; i++)
+        var midi = new MidiFile(Song.GetFullPath());
+        float bpm = 120;
+        float secondsPerQuarterNote = 60 / bpm;
+        float secondsPerTick = secondsPerQuarterNote / midi.TicksPerQuarterNote;
+
+        foreach (var track in midi.Tracks)
         {
-            Instrument instrument = Instruments[Random.Range(0, Instruments.Count)];
-            int delay = i;
-            var note = Instantiate(NotePrefab);
-            note.Time = delay + 10;
-            note.TargetPosition = instrument.transform.position;
-            note.GetComponent<Rigidbody>().MovePosition(instrument.transform.position);
-            note.UpdateTime(0f);
-            notes.Add(note);
+            foreach (var midiEvent in track.MidiEvents)
+            {
+                if (midiEvent.MidiEventType == MidiEventType.NoteOn)
+                {
+                    Instrument instrument = Instruments[track.Index % Instruments.Count];
+                    var note = Instantiate(NotePrefab);
+                    note.Time = midiEvent.Time * secondsPerTick + NoteOffset;
+                    note.TargetPosition = instrument.transform.position;
+                    note.GetComponent<Rigidbody>().MovePosition(instrument.transform.position - Vector3.back);
+                    note.UpdateTime(0f);
+                    notes.Add(note);
+                }
+            }
         }
     }
 
